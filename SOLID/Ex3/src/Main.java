f0ee984524c5db171d40cec603cdc9ddca9a3dae
THE-ASHUTOSH <ashubham1906@gmail.com>
2026-02-23 11:24:11 +0530

    ans: ans 3

--- SOLID/Ex3/src/Main.java
+++ SOLID/Ex3/src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Placement Eligibility ===");
+
+        // Setup
+        FakeEligibilityStore store = new FakeEligibilityStore();
+        EligibilityEngine engine = new EligibilityEngine(store);
+        ReportPrinter printer = new ReportPrinter();
         StudentProfile s = new StudentProfile("23BCS1001", "Ayaan", 8.10, 72, 18, LegacyFlags.NONE);
-        EligibilityEngine engine = new EligibilityEngine(new FakeEligibilityStore());
-        engine.runAndPrint(s);
+
+        // Run
+        EligibilityEngineResult result = engine.evaluate(s);
+
+        // Output & Save
+        printer.print(s, result);
+        store.save(s.rollNo, result.status);
     }
 }
