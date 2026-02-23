146d341335119d4bc3a16b1525afa548f6439857
THE-ASHUTOSH <ashubham1906@gmail.com>
2026-02-23 11:24:11 +0530

    ans: ans 3

--- SOLID/Ex3/src/EligibilityEngine.java
+++ SOLID/Ex3/src/EligibilityEngine.java
@@ -5,13 +5,6 @@ public class EligibilityEngine {
 
     public EligibilityEngine(FakeEligibilityStore store) { this.store = store; }
 
-    public void runAndPrint(StudentProfile s) {
-        ReportPrinter p = new ReportPrinter();
-        EligibilityEngineResult r = evaluate(s); // giant conditional inside
-        p.print(s, r);
-        store.save(s.rollNo, r.status);
-    }
-
     public EligibilityEngineResult evaluate(StudentProfile s) {
         List<String> reasons = new ArrayList<>();
         String status = "ELIGIBLE";
